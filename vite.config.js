649e8cd716492cdad62e437504bc3022621138e9
alexfedd <alexandrfedorov508@gmail.com>
2024-01-30 15:56:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alexfedd.github.io/react-learing/react-todo",
+  base: "/react-todo",
 })
